417dcc0f451997f3f636a6a03689d72dfcb21e27
Ceylan Yilmaz <ceylan.yilmaz@consultant.turkcell.com.tr>
2022-12-04 22:15:07 +0300

    kariyer redesign

--- components/ContactForm/index.js
+++ components/ContactForm/index.js
@@ -1,13 +1,14 @@
-import { useState } from 'react';
+import Head from 'next/head'
+import { useState, useEffect } from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import * as Yup from 'yup'
-import {useFormik} from 'formik'
+import { useFormik } from 'formik'
 
-import {FormInput, FormTextarea, Button, PhoneFormInput } from "../"
+import { FormInput, FormTextarea, Button, FormCheckbox } from "../"
 import styles from './ContactForm.module.scss';
 
-export const ContactForm = (props) => { 
+export const ContactForm = (props) => {
   const { className } = props;
 
   const contactSchema = Yup.object().shape({
@@ -15,6 +16,7 @@ export const ContactForm = (props) => {
       .email('Wrong format.')
       .required('This field cannot be left blank.'),
     namesurname: Yup.string().required('This field cannot be left blank.'),
+    recaptcha: Yup.string().required('This field cannot be left blank.'),
   })
 
   const [contact] = useState({
@@ -22,60 +24,120 @@ export const ContactForm = (props) => {
     email: '',
     phone: '',
     message: '',
+    recaptcha: ''
   })
 
   const formik = useFormik({
     initialValues: contact,
     validationSchema: contactSchema,
-    onSubmit: async (values, {setSubmitting}) => {
+    onSubmit: async (values, { setSubmitting }) => {
       setSubmitting(true)
       console.log(values)
     },
   })
-  
+
+  const verifyCallback = (response) => formik.setFieldValue('recaptcha', response);
+
+  useEffect(() => {
+    window.onloadCallback = () => {
+      try {
+        window.grecaptcha.render('captcha', {
+          'sitekey': '6LdUkLkiAAAAAK4MXrk7MXmUbxhiEWoUV8C8GxQ7',
+          'callback': verifyCallback,
+        });
+      } catch (error) {
+        console.log(error)
+      }
+    };
+  }, [])
+
   return (
     <>
+      <Head>
+        <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit" async defer></script>
+      </Head>
       <div className={classNames(styles['contact-form'], className)}>
-        <h3>Contact Us</h3>
+        <h3>Bizimle iletişime geçin</h3>
+        <div className={styles["contact-form__text"]}>
+          Lorem Ipsum is simply dummy typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book.
+        </div>
         <form onSubmit={formik.handleSubmit} noValidate>
-          <div className='form-group'>
-            <FormInput 
-              field='Full Name'
-              required
-              errorMessage={formik.errors.namesurname}
-              {...formik.getFieldProps('namesurname')}
-              className={classNames({'is-invalid': formik.touched.namesurname && formik.errors.namesurname})}
-            />
+          <div className={styles["contact-form__block"]}>
+            <div>
+              <div className='form-group'>
+                <FormInput
+                  field='Adınız'
+                  required
+                  errorMessage={formik.errors.namesurname}
+                  {...formik.getFieldProps('namesurname')}
+                  className={classNames({ 'is-invalid': formik.touched.namesurname && formik.errors.namesurname })}
+                />
+              </div>
+
+              <div className='form-group'>
+                <FormInput
+                  field='Soyadınız'
+                  required
+                  errorMessage={formik.errors.namesurname}
+                  {...formik.getFieldProps('namesurname')}
+                  className={classNames({ 'is-invalid': formik.touched.namesurname && formik.errors.namesurname })}
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className='form-group'>
+                <FormInput
+                  field='Telefon'
+                  type="phone"
+                  required
+                  errorMessage={formik.errors.phone}
+                  {...formik.getFieldProps('phone')}
+                  className={classNames({ 'is-invalid': formik.touched.phone && formik.errors.phone })}
+                />
+              </div>
+
+              <div className='form-group'>
+                <FormInput
+                  field='E-Posta'
+                  type="email"
+                  required
+                  errorMessage={formik.errors.email}
+                  {...formik.getFieldProps('email')}
+                  className={classNames({ 'is-invalid': formik.touched.email && formik.errors.email })}
+                />
+              </div>
+            </div>
           </div>
+
           <div className='form-group'>
-            <PhoneFormInput 
-              field='Phone'
-              name='phone'
+            <FormInput
+              field='Lütfen CV’nizi yükleyiniz. ( Pdf, Docx, Jpeg )'
+              type="email"
               required
-              onChange={(value) => formik.setFieldValue('phone', value)}
+              errorMessage={formik.errors.email}
+              {...formik.getFieldProps('email')}
+              className={classNames({ 'is-invalid': formik.touched.email && formik.errors.email })}
             />
           </div>
 
           <div className='form-group'>
-            <FormInput 
-              field='E-Mail'
-              type="email" 
-              required
-              errorMessage={formik.errors.email}
-              {...formik.getFieldProps('email')}
-              className={classNames({'is-invalid': formik.touched.email && formik.errors.email})}
-            />
-          </div>
-          
-          <div className='form-group'>
-            <FormTextarea 
+            <FormTextarea
               field='Message'
               rows={5}
               name={'message'}
               {...formik.getFieldProps('message')}
             />
           </div>
-          <Button text={'Submit'} button className={styles['button']} />
+          <FormCheckbox label='VOLDE tarafından yukarıda belirtmiş olduğum bilgiler üzerinden benimle iletişim kurulmasını ve izinli iletişim formu ‘nu onaylıyorum.' />
+          <div className={styles["form-group-buttons"]}>
+            <div className={classNames('captcha', { 'is-invalid': formik.touched.recaptcha && formik.errors.recaptcha })}>
+              <div id="captcha"></div>
+              <pre>{formik.errors.recaptcha}</pre>
+            </div>
+            <Button text={'Gönder'} button className={styles['button']} />
+          </div>
+
         </form>
       </div>
     </>
@@ -83,6 +145,6 @@ export const ContactForm = (props) => {
 }
 
 ContactForm.propTypes = {
-	title: PropTypes.string,
+  title: PropTypes.string,
   type: PropTypes.string
 };
---
+++ pages/kariyer/index.js
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import classNames from "classnames";
+
+import { Layout } from "../../layout";
+import styles from "../../assets/styles/Career.module.scss";
+import { ContactForm } from "../../components";
+
+export default function Career() {
+  return (
+    <>
+      <Layout>
+        <section className={classNames(styles["career"], styles["white"])}>
+          <div className={styles["career__content"]}>
+            <h2>İK Politikamız</h2>
+            <div className={"block__text"}>
+              <p>Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit.</p>
+              <p>amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis</p>
+            </div>
+          </div>
+          <div className={styles["career__image"]}>
+            <Image
+              src={"/images/img/hizmet.png"}
+              width={"940"}
+              height={"1080"}
+              alt={"Sürdürülebilirlik"}
+            />
+          </div>
+        </section>
+        <section className={styles["career"]}>
+          <div className={styles["career__content"]}>
+            <h2>Etik Uygulamalar</h2>
+            <div className={"block__text"}>
+              <p>Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit.</p>
+              <p>amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis</p>
+            </div>
+          </div>
+        </section>
+        <section className={classNames(styles["career"], styles["white"])}>
+          <div className={styles["career__content"]}>
+            <h2>İşe Alım Süreci</h2>
+            <div className={"block__text"}>
+              <p>Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit.</p>
+              <p>amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis</p>
+            </div>
+          </div>
+        </section>
+        <ContactForm />
+      </Layout>
+    </>
+  );
+}

6b0c4f979f5edcd9ac4dcc3d544110876d6d8bc8
Burhan Yılmaz <burhan.yilmaz@consultant.turkcell.com.tr>
2023-01-05 10:32:31 +0300

    revize

--- components/SocialMedia/index.js
+++ components/SocialMedia/index.js
@@ -10,10 +10,9 @@ export const SocialMedia = (props) => {
   return (
     <div className={classNames(styles['social-media'], className)}>
       <ul>
-        <li><a href="#" target="_blank" aria-label='Volde Facebook' rel="noreferrer"><Icon icon="facebook" /></a></li>
-        <li><a href="#" target="_blank" aria-label='Volde Linkedin' rel="noreferrer"><Icon icon="linkedin" /></a></li>
-        <li><a href="#" target="_blank" aria-label='Volde Instagram' rel="noreferrer"><Icon icon="instagram" /></a></li>
-        <li><a href="#" target="_blank" aria-label='Volde Youtube' rel="noreferrer"><Icon icon="youtube" /></a></li>
+        <li><a href="https://www.linkedin.com/company/voldeglobal/?viewAsMember=true" target="_blank" aria-label='Volde Linkedin' rel="noreferrer"><Icon icon="linkedin" /></a></li>
+        <li><a href="https://www.youtube.com/channel/UCcXqZk3MYmd8VvP9-339yWg" target="_blank" aria-label='Volde Youtube' rel="noreferrer"><Icon icon="youtube" /></a></li>
+        <li><a href="https://twitter.com/voldeglobal" target="_blank" aria-label='Volde Twitter' rel="noreferrer"><Icon icon="twitter" /></a></li>
       </ul>
     </div>
   )
